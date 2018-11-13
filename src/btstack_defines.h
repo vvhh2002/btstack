a8e2a0025a4106df7e594e43fce0f0bba98e5df5
Matthias Ringwald <matthias@ringwald.ch>
2018-11-13 11:51:51 +0100

    mesh: fix rebase

--- src/btstack_defines.h
+++ src/btstack_defines.h
@@ -2362,5 +2362,4 @@ typedef uint8_t sm_key_t[16];
  */
 #define MESH_PB_PROV_COMPLETE                                               0x1d
 
->>>>>>> restart mesh development based on current develop and existing mesh branches
 #endif
